1badb5c964e02f420a0257ee9342115bd99294c6
Gehan Hazem <gigi.hazem194@gmail.com>
2025-09-16 06:17:12 +0300

    Update and rename Cleaning to Cleaning.py

---
+++ Data/Cleaning.py
@@ -0,0 +1,31 @@
+ import pandas as pd
+
+#Read the raw data scraped from the website
+df = pd.read_csv("books.csv")
+
+#Clean prices to float
+df["price"] = df["price"].str.replace("£", "").astype(float)
+
+
+#Clean ratings and convert to numbers
+df["rating"] = df["rating"].str.replace("star-rating", "").str.strip()
+rating_map = {"One": 1,"Two": 2,"Three": 3,"Four": 4,"Five": 5}
+df["rating"] = df["rating"].map(rating_map)
+
+
+#Remove duplicate
+df.drop_duplicates(inplace=True)
+
+#Strip extra spaces from book titles
+df["title"] = df["title"].str.strip()
+
+#Drop rows with missing values
+df.dropna(inplace=True)
+
+
+#Save cleaned data to a new CSV file
+df.to_csv("books_clean.csv", index=False)
+
+print("\n books_clean.csv is done will work on it"
+      "  in power pi and make sql queries ✅ ")
+
